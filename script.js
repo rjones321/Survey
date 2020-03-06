b4746efdf990c3b90c2922ef326a2837af25442d
rjones321 <61877366+rjones321@users.noreply.github.com>
2020-03-06 09:37:39 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function(){
+    let age=$(".C1").val();
+    let zodiac =$(".C2").val();
+    let shower=$(".C3").val();
+$("p").text(`You are ${age} this makes sice because you are a ${zodiac}which also expains why the longest shower you had was ${shower}`);   
+    
+});
